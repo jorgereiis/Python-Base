00f6be26e57e581a1b23991244498aa1d557df9e
Jorge Reis <jreiisgalvao@gmail.com>
2022-05-25 16:33:53 -0300

    Adicionado o arquivo .gitignore

--- hello.py
+++ hello.py
@@ -5,13 +5,11 @@ Dependendo da lingua configurada no ambiente,
 o programa exibe a mensagem correspondente.
 
 Como usar:
-
 Tenha a variável LANG devidamente configurada.
 Ex:
 	export LANG=pt_BR
 
 Execução:
-
 	python3 hello.py
 	ou
 	./hello.py
